198ba5fd5b7e517e9030474f6ba1f4934dc2c720
jeremyholbrecht <Griffley@proton.me>
2024-02-09 11:16:02 +0100

    open file & read csv values

--- quiz/main.go
+++ quiz/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	
+	// open file
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(file)
+
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(file)
+	for {
+		rec, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// do something with read line
+		fmt.Println(rec)
+	}
+
+	// close file at end of program
+	defer file.Close()
 }
